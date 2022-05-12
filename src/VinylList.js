99406c7146d4365cd14998cf048b5712147b773f
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2022-05-12 14:03:29 -0400

    Cleared some comments

--- src/VinylList.js
+++ src/VinylList.js
@@ -1,18 +1,13 @@
 import React from "react";
 import Vinyl from "./Vinyl";
-import './VinylList.css'
-
-// export default function VinylList({ vinyls, toggleVinyl }) {
-//   return vinyls.map((vinyl) => {
-//     return <Vinyl key={vinyl.id} toggleVinyl={toggleVinyl} vinyl={vinyl} />;
-//   });
-// }
+import "./VinylList.css";
 
 export default function VinylList({ vinyls, toggleVinyl }) {
   return vinyls.map((vinyl) => {
-    
-    return <div className="list-item"> 
-    <Vinyl key={vinyl.id} toggleVinyl={toggleVinyl} vinyl={vinyl} />
-    </div>
+    return (
+      <div className="list-item">
+        <Vinyl key={vinyl.id} toggleVinyl={toggleVinyl} vinyl={vinyl} />
+      </div>
+    );
   });
 }
